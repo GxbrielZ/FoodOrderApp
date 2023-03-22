0a31988ee80d339d4411946e51c9fd6ca0e7fa07
Gabriel Zalewski <gabrielz2702@gmail.com>
2023-03-22 17:58:46 +0100

    Using the Context

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 import CartProvider from "./store/CartProvider";
-import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
